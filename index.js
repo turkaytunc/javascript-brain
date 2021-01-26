fbab90030e947cf037a3de9c79766587a7dfb2b4
turkaytunc <turkaytunc00@gmail.com>
2021-01-26 11:06:45 +0300

    refactor: error calculaation as a function

--- index.js
+++ index.js
@@ -1,42 +1,20 @@
 const yeniAgirlik = require('./util/yeniAgirlik');
-const errorValue = require('./util/errorValue');
-const layerErrorValue = require('./util/layerErrorValue');
 const calculateForward = require('./util/calculateForward');
-
-// calculateNetwork(
-//   [10, 30, 20],
-//   [
-//     [0.2, -0.1, 0.4],
-//     [0.7, -1.2, 1.2],
-//     [1.1, 0.1],
-//     [3.1, 1.17],
-//   ],
-//   [1, 0],
-//   0.1
-// );
+const calculateError = require('./util/calculateError');
 
 function calculateNetwork(giris, agirlik, beklenenCikis, katsayi) {
   const { h1Sigmoid, h2Sigmoid, q1Sigmoid, q2Sigmoid } = calculateForward(giris, agirlik);
 
   console.log('\nGeriye donus basladi\n');
 
-  let q1HataDegeri = errorValue(q1Sigmoid, beklenenCikis[0]);
-  console.log('q1 hata degeri = ', q1HataDegeri);
-
-  let q2HataDegeri = errorValue(q2Sigmoid, beklenenCikis[1]);
-  console.log('q2 hata degeri = ', q2HataDegeri);
-
-  let h1BagilHata = agirlik[2][0] * q1HataDegeri + agirlik[3][0] * q2HataDegeri;
-  console.log('h1 bagil hata = ', h1BagilHata);
-
-  let h1HataDegeri = layerErrorValue(h1BagilHata, h1Sigmoid);
-  console.log('h1 hata degeri = ', h1HataDegeri);
-
-  let h2BagilHata = agirlik[2][1] * q1HataDegeri + agirlik[3][1] * q2HataDegeri;
-  console.log('h2 bagil hata = ', h2BagilHata);
-
-  let h2HataDegeri = layerErrorValue(h2BagilHata, h2Sigmoid);
-  console.log('h2 hata degeri = ', h2HataDegeri);
+  const { h1HataDegeri, h2HataDegeri, q1HataDegeri, q2HataDegeri } = calculateError(
+    q1Sigmoid,
+    q2Sigmoid,
+    h1Sigmoid,
+    h2Sigmoid,
+    agirlik,
+    beklenenCikis
+  );
 
   console.log('\nYeni agirliklar\n');
 
---
+++ util/calculateError.js
@@ -0,0 +1,26 @@
+const errorValue = require('./errorValue');
+const layerErrorValue = require('./layerErrorValue');
+
+function calculateError(q1Sigmoid, q2Sigmoid, h1Sigmoid, h2Sigmoid, agirlik, beklenenCikis) {
+  let q1HataDegeri = errorValue(q1Sigmoid, beklenenCikis[0]);
+  console.log('q1 hata degeri = ', q1HataDegeri);
+
+  let q2HataDegeri = errorValue(q2Sigmoid, beklenenCikis[1]);
+  console.log('q2 hata degeri = ', q2HataDegeri);
+
+  let h1BagilHata = agirlik[2][0] * q1HataDegeri + agirlik[3][0] * q2HataDegeri;
+  console.log('h1 bagil hata = ', h1BagilHata);
+
+  let h1HataDegeri = layerErrorValue(h1BagilHata, h1Sigmoid);
+  console.log('h1 hata degeri = ', h1HataDegeri);
+
+  let h2BagilHata = agirlik[2][1] * q1HataDegeri + agirlik[3][1] * q2HataDegeri;
+  console.log('h2 bagil hata = ', h2BagilHata);
+
+  let h2HataDegeri = layerErrorValue(h2BagilHata, h2Sigmoid);
+  console.log('h2 hata degeri = ', h2HataDegeri);
+
+  return { h1HataDegeri, h2HataDegeri, q1HataDegeri, q2HataDegeri };
+}
+
+module.exports = calculateError;
